cd8f39e54191d814ed2444dff73acd08df1d7249
Safiullah Abbasi <safiabbasi402@gmail.com>
2026-07-23 16:48:01 +0500

    Project-01 Uploaded Successfully

---
+++ project-01/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Application</h1>
+      <p>How to Create a React app</p>
+      <ul>
+        <li>Run Command on Terminal (npm create vite)</li>
+        <li>Choose project name</li>
+        <li>Select React</li>
+        <li>Select Javascript</li>
+        <li>You are Good to GO Now, React application created Successfully</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
