6f6e4b5a66b5ff4dff583326b3a0beed70fa9198
Safiullah Abbasi <safiabbasi402@gmail.com>
2026-08-08 23:39:36 +0500

    Project 05 Folder Added

---
+++ project-05_Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='mx-auto text-white h-screen w-full bg-gray-900 flex items-center justify-center font-bold text-3xl'>
+      Styling Using Tailwind CSS
+    </div>
+  )
+}
+
+export default App
