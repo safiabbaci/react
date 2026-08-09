8c7e17759c7510df37bc572bb4c616092b805055
Safiullah Abbasi <safiabbasi402@gmail.com>
2026-08-09 22:21:35 +0500

    Update Project 11 Folder

--- project-11_NotesApplication/src/App.jsx
+++ project-11_NotesApplication/src/App.jsx
@@ -34,9 +34,9 @@ const App = () => {
         className="w-full min-h-screen flex md:flex-row max-md:flex-col justify-center"
       >
         <div className="md:w-[45%] max-md:w-full flex flex-col items-center gap-5 p-10">
-          <h1 className="text-3xl mb-[20px] font-medium">Notes Application</h1>
+          <h1 className="text-3xl mb-5 font-medium">Notes Application</h1>
           <input
-            className="font-medium w-full px-5 py-2 border-1 rounded outline-none max-md:w-[320px]"
+            className="font-medium w-full px-5 py-2 border rounded outline-none max-md:w-[320px]"
             type="text"
             placeholder="Enter Notes Heading"
             value={title}
@@ -45,7 +45,7 @@ const App = () => {
             }}
           />
           <textarea
-            className="font-medium w-full h-30 px-5 py-3 border-1 rounded outline-non max-md:w-[320px]"
+            className="font-medium w-full h-30 px-5 py-3 border rounded outline-non max-md:w-[320px]"
             type="text"
             placeholder="Write Detail"
             value={description}
@@ -53,13 +53,13 @@ const App = () => {
               setDescription(e.target.value);
             }}
           ></textarea>
-          <button className="max-md:w-[320px] font-medium w-full px-5 py-2 border-1 rounded-xl width-content text-black bg-white cursor-pointer hover:bg-black hover:text-white outline-none duration-300 active:bg-gray-300 active:text-black active:scale-95">
+          <button className="max-md:w-[320px] font-medium w-full px-5 py-2 border rounded-xl width-content text-black bg-white cursor-pointer hover:bg-black hover:text-white outline-none duration-300 active:bg-gray-300 active:text-black active:scale-95">
             Add Notes
           </button>
         </div>
-        <div className="md:w-[55%] sm:w-full flex flex-col items-start gap-5 p-10 border-white min-md:border-l-2 max-md:border-t-2">
-          <h1 className="text-3xl mb-[20px] font-medium">Recent Notes</h1>
-          <div className="min-md:max-h-110 grid min-lg:grid-cols-3 min-md:grid-cols-2 min-sm:grid-cols-3 max-sm:grid-cols-2 w-full gap-5 overflow-auto max-md:overflow-visible scrollbar-gutter-stable">
+        <div className="md:w-[55%] sm:w-full flex flex-col items-start gap-5 p-10 border-white md:border-l-2 max-md:border-t-2">
+          <h1 className="text-3xl mb-5 font-medium">Recent Notes</h1>
+          <div className="md:max-h-110 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-3 max-sm:grid-cols-2 w-full gap-5 overflow-auto max-md:overflow-visible scrollbar-gutter-stable">
             {task.map(function (elem, idx) {
               return (
                 <div
@@ -74,7 +74,7 @@ const App = () => {
                   >
                     X
                   </div>
-                  <h1 className="uppercase text-[18px] mb-[10px] font-bold">
+                  <h1 className="uppercase text-[18px] mb-2.5 font-bold">
                     {elem.title}
                   </h1>
                   <p className="text-gray-600">{elem.description}</p>
