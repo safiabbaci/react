8d3ba248a6ad3c9c7e94fae8baa26f9dafa449b8
Safiullah Abbasi <safiabbasi402@gmail.com>
2026-08-10 23:33:44 +0500

    Upload Project 12 Folder

---
+++ project-12_LocalStorage/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const App = () => {
+
+  //Add Items to Local Storage
+  // localStorage.setItem('user', 'Safiullah Abbasi')
+  // localStorage.setItem('age', 21)
+  // localStorage.setItem('gender', 'Male')
+
+  //Get Items From Local Storage
+  // const user = localStorage.getItem('user')
+  // const age = localStorage.getItem('age')
+  // const gender = localStorage.getItem('gender')
+
+  // console.log("User : ", user);
+  // console.log("Age : ", age);
+  // console.log("Gender : ", gender);
+  
+  //Remove Items From Local Storage
+  // localStorage.removeItem('user');
+
+  // Clear All the items from Local Storage
+  // localStorage.clear();
+
+  //Store and Retrieve an Object From Local Storage
+  // const user = {
+  //   name: 'Safiullah Abbasi',
+  //   age: 21,
+  //   gender: 'Male'
+  // }
+
+  // localStorage.setItem('user', JSON.stringify(user));
+
+  // const userData = localStorage.getItem('user');
+  // console.log(JSON.parse(userData));
+ 
+
+  //Store and Retrieve an array from Local Storage
+  // const arr = [10,20,30,40];
+
+  // localStorage.setItem('Array', JSON.stringify(arr));
+
+  // const userData = localStorage.getItem('Array');
+  // console.log(JSON.parse(userData));
+  
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
